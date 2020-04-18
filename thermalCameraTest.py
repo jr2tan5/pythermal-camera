f58dfa091a52195eefe48fb4725636c288e58a33
jr2tan5 <jr2tan.5@gmail.com>
2020-04-18 23:55:50 +0800

    Modified Button Pressing and Created Video Record Function

--- thermalCameraTest.py
+++ thermalCameraTest.py
@@ -4,21 +4,28 @@ import datetime
 import time
 import os
 
+# Global Variable
+videoCapture_Status = 0x0
+screenshot_button_pressed = False
+video_button_pressed = False
+
 # Assign Functions to Variables
 timenow = datetime.datetime.now
 
 # Camera Enum by Raspberry Pi
 rasp_pi_camera = 0
-thermal_camera = 2
+thermal_camera = 1
 
 # Set GPIO Object Enum
-RASP_PI_CAM_STATUS_LED_EN = 18 # Raspberry Pi Camera Readability Status LED is connected to GPIO 18
-THERMAL_CAM_STATUS_LED_EN = 17 # Thermal Camera Readability Status LED is connected to GPIO 17
-SCREENSHOT_BUTTON_EN = 21 # Screenshot Button is connected to GPIO21
-VIDEO_START_STOP_BUTTON_EN = 20 # Video Start/Stop Button is connected to GPIO20
+RASP_PI_CAM_STATUS_LED_EN = 18 # Raspberry Pi Camera Readability Status LED is connected to GPIO18 of the raspberry pi
+THERMAL_CAM_STATUS_LED_EN = 17 # Thermal Camera Readability Status LED is connected to GPIO17 of the raspberry pi
+VIDEO_CAPTURE_STATUS_LED_EN = 16 # Video Capture Status LED is connected to GPIO16 of the raspberry pi
+SCREENSHOT_BUTTON_EN = 21 # Screenshot Button is connected to GPIO21 of the raspberry pi
+VIDEO_START_STOP_BUTTON_EN = 20 # Video Start/Stop Button is connected to GPIO20 of the raspberry pi
 
 # File Paths
 screenshot_folder = '/home/pi/Desktop/pythermal-camera-git/Screenshots'
+video_folder = '/home/pi/Desktop/pythermal-camera-git/Video'
 
 # Create OpenCV UI object
 cv2.namedWindow("raspberry_pi_camera_v1_3")
@@ -34,6 +41,7 @@ GPIO.setwarnings(False)
 # LED Status Lights
 GPIO.setup(RASP_PI_CAM_STATUS_LED_EN,GPIO.OUT)
 GPIO.setup(THERMAL_CAM_STATUS_LED_EN,GPIO.OUT)
+GPIO.setup(VIDEO_CAPTURE_STATUS_LED_EN,GPIO.OUT)
 
 # Push Button Inputs
 GPIO.setup(SCREENSHOT_BUTTON_EN,GPIO.IN)
@@ -41,7 +49,9 @@ GPIO.setup(VIDEO_START_STOP_BUTTON_EN,GPIO.IN)
 
 # Create Video Capture Object
 vc_normal = cv2.VideoCapture(rasp_pi_camera)
+print('Found vc_normal')
 vc_thermal = cv2.VideoCapture(thermal_camera)
+print('Found vc_thermal')
 
 def turnOnOrOffStatus_LED(cameraStatus, cameraLED_GPIO_Name):
     if cameraStatus:
@@ -58,6 +68,23 @@ def saveImages(normal_frame, thermal_frame):
     cv2.imwrite(screenshot_folder + "/" + str(time) + ".jpg",normal_frame)
     cv2.imwrite(screenshot_folder + "/" + str(time) + "_thermal.jpg",thermal_frame)
 
+def saveVideo(normal_writer, thermal_writer,normal_frame, thermal_frame):
+    # Check if video folder exits:
+    if not os.path.exists(video_folder):
+        os.mkdir(video_folder)
+    normal_writer.write(normal_frame)
+    thermal_writer.write(thermal_frame)
+    
+def getVideoWriter(videoName, dimensions):
+    return cv2.VideoWriter(video_folder + "/" + videoName, cv2.VideoWriter_fourcc(*'XVID'), 10, dimensions)
+
+def init():
+    turnOnOrOffStatus_LED(False, THERMAL_CAM_STATUS_LED_EN)
+    turnOnOrOffStatus_LED(False, RASP_PI_CAM_STATUS_LED_EN)
+    turnOnOrOffStatus_LED(False, VIDEO_CAPTURE_STATUS_LED_EN)
+
+init()
+
 if vc_normal.isOpened(): # try to get the first frame
     normal_cam_status, normal_frame = vc_normal.read()
     turnOnOrOffStatus_LED(normal_cam_status, RASP_PI_CAM_STATUS_LED_EN)
@@ -72,12 +99,17 @@ if vc_thermal.isOpened(): # try to get the first frame
     thermal_frame_width = int(thermal_frame.shape[1] * scale_percent / 100)
     thermal_frame_height = int(thermal_frame.shape[0] * scale_percent / 100)
     
+    if thermal_frame_width > 640:
+        thermal_frame_width /= 4
+        thermal_frame_height /=4
     # Save desired resolution dimensions
     thermal_frame_dim = (thermal_frame_width, thermal_frame_height)
+    
 else:
     thermal_cam_status = False
 
 while normal_cam_status and thermal_cam_status:
+    timeStart = time.time()
     # Get Status and Frames from camera
     normal_cam_status, normal_frame = vc_normal.read()
     turnOnOrOffStatus_LED(normal_cam_status, RASP_PI_CAM_STATUS_LED_EN)
@@ -92,13 +124,54 @@ while normal_cam_status and thermal_cam_status:
     cv2.imshow("raspberry_pi_camera_v1_3", normal_frame_flipped_180)
     cv2.imshow("flir_thermal_camera", thermal_frame_resized)
     
-    # Screenshot purposes 
+    # Screenshot Purposes
     if GPIO.input(SCREENSHOT_BUTTON_EN) == GPIO.HIGH: # Check if screenshot button is pressed
-        saveImages(normal_frame_flipped_180, thermal_frame_resized) 
-        print "screenshot taken" # Used for debugging
+        screenshot_button_pressed = True
+    else:
+        # This Ensures that there is only one input per button press
+        if screenshot_button_pressed == True:
+            saveImages(normal_frame_flipped_180, thermal_frame_resized) 
+            print "screenshot taken" # Used for debugging
+            screenshot_button_pressed = False
+    
+    # Video Saving Purposes
+    if GPIO.input(VIDEO_START_STOP_BUTTON_EN) == GPIO.HIGH:
+        video_button_pressed = True
+    else:
+        # This Ensures that there is only one input per button press
+        if video_button_pressed == True:
+            print('video record pressed')
+            
+            # Stop Recording Procedures if Button is pressed
+            if videoCapture_Status:
+                normal_video_writer.release()
+                thermal_video_writer.release()
+                
+            # Toggle Start/Stop when button is pressed
+            videoCapture_Status ^= 0x1
+            
+            # Start Recording Procedures if Button is pressed
+            if videoCapture_Status:
+                time = str(timenow())
+                normal_video_filename = time + ".avi"
+                thermal_video_filename = time + "_thermal.avi"
+                normal_video_writer = getVideoWriter(normal_video_filename, thermal_frame_dim)
+                thermal_video_writer = getVideoWriter(thermal_video_filename, thermal_frame_dim)
+            
+            video_button_pressed = False
+    
+    # Save the Frame while video is recording
+    if videoCapture_Status:
+        saveVideo(normal_video_writer, thermal_video_writer, normal_frame_flipped_180, thermal_frame_resized)
+    
+    # Show the current recording status on the LED
+    turnOnOrOffStatus_LED(videoCapture_Status, VIDEO_CAPTURE_STATUS_LED_EN)
+        
     
     # Delay to show frame for desired duration before reading the next frame
     key = cv2.waitKey(10)
     
     if key == 27: # exit on ESC
         break
+    timeEnd = time.time()
+    print(timeStart - timeEnd)
